1d74dbfed1c8d69d38f8cdd7c4aef95e5c7414e5
therollingambit <therollingambit@gmail.com>
2023-03-24 01:11:54 +0800

    refactor

--- src/server/api/routers/topic.ts
+++ src/server/api/routers/topic.ts
@@ -4,21 +4,21 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const topicRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
-      return ctx.prisma.topic.findMany({
-        where: {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+
+  create: protectedProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.topic.create({
+        data: {
+          title: input.title,
           userId: ctx.session.user.id,
         },
       });
     }),
-
-  create: protectedProcedure
-    .input(z.object({ title: z.string() }))
-    .mutation(({ ctx, input })) => {
-      return ctx.prisma.topic.create({
-        data: {
-          title: input.title,
-          userId: ctx.session.user.id
-        }
-      })
-    }
 });
